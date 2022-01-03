3c5ff103acbbafba920b18db976329346f2bbf8f
Himanshu <hv65616@gmail.com>
2022-01-04 01:41:16 +0530

    Adding of news card and a header

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,8 +4,31 @@ import NewsItem from "./NewsItem";
 export default class News extends Component {
   render() {
     return (
-      <div>
-        <NewsItem></NewsItem>
+      <div className="container my-3">
+        <h3
+          style={{
+            marginBottom: "10px",
+            paddingTop: "5px",
+            paddingBottom: "5px",
+            marginTop: "10px",
+          }}
+        >
+          NewsInshort - Top Headlines
+        </h3>
+        <div className="row">
+          <div className="col-md-3">
+            <NewsItem title="Mytitle" description="lorem100"></NewsItem>
+          </div>
+          <div className="col-md-3">
+            <NewsItem title="Mytitle" description="lorem100"></NewsItem>
+          </div>
+          <div className="col-md-3">
+            <NewsItem title="Mytitle" description="lorem100"></NewsItem>
+          </div>
+          <div className="col-md-3">
+            <NewsItem title="Mytitle" description="lorem100"></NewsItem>
+          </div>
+        </div>
       </div>
     );
   }
