0a2cdb5339fd1c9588507a71e1ea5907e023c64b
Himanshu <hv65616@gmail.com>
2022-01-04 18:10:05 +0530

    Image sourse is a javascript

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,23 +2,21 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageUrl } = this.props;
     return (
       <div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img
-            src="https://blogger.googleusercontent.com/img/a/AVvXsEik_48hPzMzsDzwfdUeHj4jNGqYGevEuVjTTPkAGTu9bRN3oePxV6bxF897GK8Az3AaSqUOalYXNG4HSCy0fW5KUHuruCWP8hAfZxgrgbzh-dsbLM9jxyFGCthOZdBCa1dNkqk6mrVl0VtflsV2VvKXfGnwL6-68m-mxp7qHJuLlvqGIahZ9YDe5mt97w=w1200-h630-p-k-no-nu"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
-            </a>
+        <div className="my-3">
+          <div className="card" style={{ width: "18rem" }}>
+            <img src={imageUrl} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <a href="/" className="btn btn-sm btn-primary">
+                Read More...
+              </a>
+            </div>
           </div>
-        </div>
+        </div>  
       </div>
     );
   }
