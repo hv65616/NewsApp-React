3acabbb96d7d2df95789d35351d864051ac6e03d
Himanshu <hv65616@gmail.com>
2022-01-05 11:36:45 +0530

    Changes in newsurl

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,21 +2,26 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl } = this.props;
+    let { title, description, imageUrl, newsurl } = this.props;
     return (
       <div>
         <div className="my-3">
           <div className="card" style={{ width: "18rem" }}>
             <img src={imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}</p>
-              <a href="/" className="btn btn-sm btn-primary">
+              <h5 className="card-title">{title}...</h5>
+              <p className="card-text">{description}...</p>
+              <a
+                href={newsurl}
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-sm btn-primary"
+              >
                 Read More...
               </a>
             </div>
           </div>
-        </div>  
+        </div>
       </div>
     );
   }
