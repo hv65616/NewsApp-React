4e576153c6d255589c9556da560c10abc1cc859a
Himanshu <hv65616@gmail.com>
2022-01-07 15:06:27 +0530

    Image src updatation for if image url is null

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -7,7 +7,15 @@ export default class NewsItem extends Component {
       <div>
         <div className="my-3">
           <div className="card" style={{ width: "18rem" }}>
-            <img src={imageUrl} className="card-img-top" alt="..." />
+            <img
+              src={
+                !imageUrl
+                  ? "https://content.fortune.com/wp-content/uploads/2022/01/GettyImages-1237544263-e1641528864154.jpeg?resize=1200,600"
+                  : imageUrl
+              }
+              className="card-img-top"
+              alt="..."
+            />
             <div className="card-body">
               <h5 className="card-title">{title}...</h5>
               <p className="card-text">{description}...</p>
