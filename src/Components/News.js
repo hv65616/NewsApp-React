83d194bdb7ed42389c4e7f6999e8d9e324c92f53
Himanshu <hv65616@gmail.com>
2022-01-09 03:32:51 +0530

    Adding of next and previous buttons with its proper functionality

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,16 +4,46 @@ import NewsItem from "./NewsItem";
 export default class News extends Component {
   constructor() {
     super();
-    this.state = { articles: [], loading: false };
+    this.state = { articles: [], loading: false, page: 1 };
   }
 
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d919b28ed32b43baad717e2c2d12d5ff";
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d919b28ed32b43baad717e2c2d12d5ff&page=1&pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles });
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
+
+  previousHandleClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d919b28ed32b43baad717e2c2d12d5ff&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+  
+  nextHandleClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d919b28ed32b43baad717e2c2d12d5ff&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
 
   render() {
     return (
@@ -41,6 +71,23 @@ export default class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.previousHandleClick}
+            disabled={this.state.page <= 1}
+          >
+            Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.nextHandleClick}
+          >
+            Next
+          </button>
         </div>
       </div>
     );
