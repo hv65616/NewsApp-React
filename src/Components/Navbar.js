41dcb09cb18928c2c6d9807b34e8f1ffa13b6d35
Himanshu <hv65616@gmail.com>
2022-01-09 03:38:05 +0530

    Darking of navigation bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <a className="navbar-brand" href="/">
               NewsInshort
