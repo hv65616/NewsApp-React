284170fde34d5436e85f0a5a56ae794b0f9a8765
Himanshu <hv65616@gmail.com>
2022-01-11 17:49:58 +0530

    Code of react router on routing multiple pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,44 @@ import "./App.css";
 import React, { Component } from "react";
 import NavBar from "./Components/Navbar";
 import News from "./Components/News";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 export default class App extends Component {
   render() {
     return (
       <div>
-        <NavBar></NavBar>
-        <News pageSize = {8} category = "health"></News>
+        <Router>
+          <NavBar></NavBar>
+          <Switch>
+            <Route exact path="/">
+              <News key="general" pageSize={8} category="general"></News>
+            </Route>
+            <Route exact path="/general">
+              <News key="general" pageSize={8} category="general"></News>
+            </Route>
+            <Route exact path="/business">
+              <News key="business" pageSize={8} category="business"></News>
+            </Route>
+            <Route exact path="/entertainment">
+              <News
+                key="entertainment"
+                pageSize={8}
+                category="entertainment"
+              ></News>
+            </Route>
+            <Route exact path="/health">
+              <News key="health" pageSize={8} category="health"></News>
+            </Route>
+            <Route exact path="/science">
+              <News key="science" pageSize={8} category="science"></News>
+            </Route>
+            <Route exact path="/sports">
+              <News key="sports" pageSize={8} category="sports"></News>
+            </Route>
+            <Route exact path="/technology">
+              <News key="technology" pageSize={8} category="technology"></News>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
