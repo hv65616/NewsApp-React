f1e554f46c3b58119f93db9905f1a9a9e809a57c
Himanshu <hv65616@gmail.com>
2022-01-12 14:52:00 +0530

    Created pagesize variable and pass it in evry call

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./Components/Navbar";
 import News from "./Components/News";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 export default class App extends Component {
+  pageSize = 8;
   render() {
     return (
       <div>
@@ -11,32 +12,60 @@ export default class App extends Component {
           <NavBar></NavBar>
           <Switch>
             <Route exact path="/">
-              <News key="general" pageSize={8} category="general"></News>
+              <News
+                key="general"
+                pageSize={this.pageSize}
+                category="general"
+              ></News>
             </Route>
             <Route exact path="/general">
-              <News key="general" pageSize={8} category="general"></News>
+              <News
+                key="general"
+                pageSize={this.pageSize}
+                category="general"
+              ></News>
             </Route>
             <Route exact path="/business">
-              <News key="business" pageSize={8} category="business"></News>
+              <News
+                key="business"
+                pageSize={this.pageSize}
+                category="business"
+              ></News>
             </Route>
             <Route exact path="/entertainment">
               <News
                 key="entertainment"
-                pageSize={8}
+                pageSize={this.pageSize}
                 category="entertainment"
               ></News>
             </Route>
             <Route exact path="/health">
-              <News key="health" pageSize={8} category="health"></News>
+              <News
+                key="health"
+                pageSize={this.pageSize}
+                category="health"
+              ></News>
             </Route>
             <Route exact path="/science">
-              <News key="science" pageSize={8} category="science"></News>
+              <News
+                key="science"
+                pageSize={this.pageSize}
+                category="science"
+              ></News>
             </Route>
             <Route exact path="/sports">
-              <News key="sports" pageSize={8} category="sports"></News>
+              <News
+                key="sports"
+                pageSize={this.pageSize}
+                category="sports"
+              ></News>
             </Route>
             <Route exact path="/technology">
-              <News key="technology" pageSize={8} category="technology"></News>
+              <News
+                key="technology"
+                pageSize={this.pageSize}
+                category="technology"
+              ></News>
             </Route>
           </Switch>
         </Router>
