c870259a8a9e2f7b7c8ef51b895cd2d8699a4f97
Himanshu <hv65616@gmail.com>
2022-01-13 23:53:20 +0530

    Aligning of spinner in centre

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -3,7 +3,7 @@ import spinner from "./loading.gif";
 export default class Spinner extends Component {
   render() {
     return (
-      <div>
+      <div className="text-center">
         <img src={spinner} alt="spinner" />
       </div>
     );
