25e1f05d57b20ee75a02b85d8a42495794267dc9
Himanshu <hv65616@gmail.com>
2022-01-14 00:44:32 +0530

    Loading Bar Component added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import React, { Component } from "react";
 import NavBar from "./Components/Navbar";
 import News from "./Components/News";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 export default class App extends Component {
   pageSize = 8;
+  state = {
+    progress: 10,
+  };
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
   render() {
     return (
       <div>
         <Router>
           <NavBar></NavBar>
+          <LoadingBar
+            color="#f11946"
+            progress={this.state.progress}
+            height={3}
+            shadow={true}
+          />
+
           <Switch>
             <Route exact path="/">
               <News
+                setProgress={this.setProgress}
                 key="general"
                 pageSize={this.pageSize}
                 category="general"
@@ -20,6 +35,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/general">
               <News
+                setProgress={this.setProgress}
                 key="general"
                 pageSize={this.pageSize}
                 category="general"
@@ -27,6 +43,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/business">
               <News
+                setProgress={this.setProgress}
                 key="business"
                 pageSize={this.pageSize}
                 category="business"
@@ -34,6 +51,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/entertainment">
               <News
+                setProgress={this.setProgress}
                 key="entertainment"
                 pageSize={this.pageSize}
                 category="entertainment"
@@ -41,6 +59,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/health">
               <News
+                setProgress={this.setProgress}
                 key="health"
                 pageSize={this.pageSize}
                 category="health"
@@ -48,6 +67,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/science">
               <News
+                setProgress={this.setProgress}
                 key="science"
                 pageSize={this.pageSize}
                 category="science"
@@ -55,6 +75,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/sports">
               <News
+                setProgress={this.setProgress}
                 key="sports"
                 pageSize={this.pageSize}
                 category="sports"
@@ -62,6 +83,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/technology">
               <News
+                setProgress={this.setProgress}
                 key="technology"
                 pageSize={this.pageSize}
                 category="technology"
