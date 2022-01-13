05ef256f99314b66904fc9538f240c11989764f6
Himanshu <hv65616@gmail.com>
2022-01-13 23:53:48 +0530

    Replacing the next prev button with infinte scroll

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
   static defaultProps = {
@@ -21,8 +22,10 @@ export default class News extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = { articles: [], loading: false, page: 1 };
-    document.title = `${this.capitalizeFirstLetter(this.props.category)}-NewsInshort`;
+    this.state = { articles: [], loading: false, page: 1, totalResults: 0 };
+    document.title = `${this.capitalizeFirstLetter(
+      this.props.category
+    )}-NewsInshort`;
   }
 
   async updatedData() {
@@ -93,9 +96,23 @@ export default class News extends Component {
     }
   };
 
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=d919b28ed32b43baad717e2c2d12d5ff&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  };
+
   render() {
     return (
-      <div className="container my-3">
+      <>
         <h3
           style={{
             textAlign: "center",
@@ -105,30 +122,41 @@ export default class News extends Component {
             marginTop: "10px",
           }}
         >
-          NewsInshort - Top {this.capitalizeFirstLetter(this.props.category)} HeadLines
+          NewsInshort - Top {this.capitalizeFirstLetter(this.props.category)}{" "}
+          HeadLines
         </h3>
-        {this.state.loading && <Spinner></Spinner>}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div className="col-md-3" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title : " "}
-                    description={
-                      element.description ? element.description : " "
-                    }
-                    imageUrl={element.urlToImage}
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  ></NewsItem>
-                </div>
-              );
-            })}
+        {/* {this.state.loading && <Spinner></Spinner>} */}
+        <div className="container">
+          <InfiniteScroll
+            dataLength={this.state.articles.length}
+            next={this.fetchMoreData}
+            hasMore={
+              this.state.articles.length !== this.state.totalResults.length
+            }
+            loader={<Spinner></Spinner>}
+          >
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-3" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title : " "}
+                      description={
+                        element.description ? element.description : " "
+                      }
+                      imageUrl={element.urlToImage}
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    ></NewsItem>
+                  </div>
+                );
+              })}
+            </div>
+          </InfiniteScroll>
         </div>
-        <div className="d-flex justify-content-between">
+        {/* <div className="d-flex justify-content-between">
           <button
             type="button"
             className="btn btn-dark"
@@ -148,8 +176,8 @@ export default class News extends Component {
           >
             Next
           </button>
-        </div>
-      </div>
+        </div> */}
+      </>
     );
   }
 }
